1fd02165b8e45014ff4c8ffadd28cbfa8e8cf0b6
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-11-07 10:54:33 +0500

    first version may contain errors

--- src/middleware/check.tokens.js
+++ src/middleware/check.tokens.js
@@ -2,12 +2,23 @@ import { verifyTokens } from "../helpers/jwt.js";
 
 export const checkTokens = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ error: "Authorization header is not found" });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "Token is not found" });
+    }
     const { error, decode } = verifyTokens("access", token);
-    req.use = decode;
+    if (error) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    req.user = decode;
     next();
   } catch (err) {
-    next(err);
+    console.error("Error in token verification:", err);
+    res.status(500).json({ error: "Server error while verifying token" });
   }
 };
